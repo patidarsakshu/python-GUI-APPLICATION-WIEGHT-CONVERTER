dc5c878e241a85fd301dfc85f3a3a3b0d364c9f0
sakshi patidar <103515914+patidarsakshu@users.noreply.github.com>
2022-12-14 23:28:54 +0530

    Delete first.py

--- python GUI/first.py
+++
@@ -1,9 +0,0 @@
-from cgitb import text
-from tkinter import *
-root=Tk()
-root.title("my first GUI application")
-root.geometry('400x400')
-root.minsize(200 ,100)
-sakshi=Label(text=" Anirban sir ka project jo ki GUI pr banana hai!! ")
-sakshi.pack()
-root.mainloop()
